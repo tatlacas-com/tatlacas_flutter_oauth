26599d9c2a48a7b2bb41eee4b53c7862c80a6bee
tatlacas <tathove@gmail.com>
2021-05-04 20:18:43 +0200

    macos and windows

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import tatlacas_flutter_oauth
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  TatlacasFlutterOauthPlugin.register(with: registry.registrar(forPlugin: "TatlacasFlutterOauthPlugin"))
+}
