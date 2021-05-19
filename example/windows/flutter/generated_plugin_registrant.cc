4c6b0395495c1378b11d616cac81dfb11733e646
Tatenda <tathove@gmail.com>
2021-05-19 19:44:20 +0200

    windows updates

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <tatlacas_flutter_oauth/tatlacas_flutter_oauth_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  TatlacasFlutterOauthPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("TatlacasFlutterOauthPlugin"));
+}
